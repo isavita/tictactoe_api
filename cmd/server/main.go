c53f2a9b95911d9f454254a82b2c3069cd397c5a
Aleksandar Dimov <isavitaisa@gmail.com>
2023-05-13 16:53:21 +0100

    Add endpoint for ai-plugin.json and openapi.yaml

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -15,6 +16,15 @@ func main() {
 
 	http.HandleFunc("/v1/tictactoe", ticTacToeAPI.TicTacToeHandler)
 
+	// Handle ai-plugin.json request for OpenAI Plugins.
+	http.HandleFunc("/.well-known/ai-plugin.json", openAIPluginHandler)
+
+	// Handle openapi.yaml request.
+	http.HandleFunc("/openapi.yaml", openapiHandler)
+
+	// Handle logo.png request.
+	http.HandleFunc("/logo.png", logoHandler)
+
 	// Handle all other requests with a 404 Not Found.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("404 - %s", r.URL.Path)
@@ -28,4 +38,58 @@ func main() {
 	}
 
 	http.ListenAndServe(":"+port, nil)
+}
+
+func openAIPluginHandler(w http.ResponseWriter, r *http.Request) {
+	jsonFile, err := os.Open(".well-known/ai-plugin.json") // replace with the path to your ai-plugin.json file
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer jsonFile.Close()
+
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func logoHandler(w http.ResponseWriter, r *http.Request) {
+	logoFile, err := os.Open(".well-known/logo.png")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer logoFile.Close()
+
+	logoData, err := ioutil.ReadAll(logoFile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(logoData)
+}
+
+func openapiHandler(w http.ResponseWriter, r *http.Request) {
+	openapiFile, err := os.Open(".well-known/openapi.yaml")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer openapiFile.Close()
+
+	openapiData, err := ioutil.ReadAll(openapiFile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/yaml")
+	w.Write(openapiData)
 }
